bd352ecab9850fc8f68a2522200f088c35d7848d
Kaustubh Pathak <kaustuap555@gmail.com>
2024-01-18 13:25:27 +0530

    added

---
+++ maths/armstrong.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    int d;
+    int s = 0;
+
+    for (int i = n; i != 0; i = i / 10)
+    {
+        d = i % 10;
+        s = s + (d * d * d);
+    }
+    
+    if (s == n)
+    {
+        cout << "Armstrong Number " << endl;
+    }
+    else
+    {
+        cout << "Not an Armstrong Number" << endl;
+    }
+}
